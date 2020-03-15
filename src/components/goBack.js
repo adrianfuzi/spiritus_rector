0c41b58675e7a7a29fad1bcf35ebbcefab1c7979
adrianfuzi <37154180+adewfalcon@users.noreply.github.com>
2020-03-15 15:32:22 +0100

    Fix #1 go-back button

--- src/components/goBack.js
+++ src/components/goBack.js
@@ -3,11 +3,13 @@ import React from "react"
 import backArrow from "../images/backArrow.svg"
 
 const GoBack = () => {
-  return window.history.length > 2 ? (
-    <button className="back" onClick={() => window.history.back()}>
-      <img className="back__arrow" src={backArrow} alt=""/> Vissza
-    </button>
-  ) : null
+    if (typeof window !== `undefined`) {
+        return window.history.length > 2 ? (
+          <button className="back" onClick={() => window.history.back()}>
+            <img className="back__arrow" src={backArrow} alt=""/> Vissza
+          </button>
+        ) : null
+    }
 }
 
 export default GoBack
